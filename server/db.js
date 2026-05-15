ea0456dd94f5335358ae02e6f20c4dc1a55f4e76
layd64 <burakovstanislav64@gmail.com>
2026-05-16 02:57:30 +0300

    lab6: add db_uri support

--- server/db.js
+++ server/db.js
@@ -1,22 +1,33 @@
 const { Sequelize } = require('sequelize');
 
 // підключення до бд через змінні середовища
-const sequelize = new Sequelize(
-    process.env.DB_NAME || 'web_backend_lab',
-    process.env.DB_USER || 'root',
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 3306,
+const sequelize = process.env.DB_URI 
+    ? new Sequelize(process.env.DB_URI, {
         dialect: 'mysql',
-        logging: false, // вимкнути логування sql-запитів у консоль
-        dialectOptions: process.env.DB_HOST && process.env.DB_HOST.includes('aivencloud') ? {
+        logging: false,
+        dialectOptions: process.env.DB_URI.includes('aivencloud') ? {
             ssl: {
                 require: true,
                 rejectUnauthorized: false
             }
         } : {}
-    }
-);
+    })
+    : new Sequelize(
+        process.env.DB_NAME || 'web_backend_lab',
+        process.env.DB_USER || 'root',
+        process.env.DB_PASS,
+        {
+            host: process.env.DB_HOST || 'localhost',
+            port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+            dialect: 'mysql',
+            logging: false,
+            dialectOptions: process.env.DB_HOST && process.env.DB_HOST.includes('aivencloud') ? {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            } : {}
+        }
+    );
 
 module.exports = sequelize;
